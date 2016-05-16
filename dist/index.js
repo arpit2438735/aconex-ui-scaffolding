fe0235fadbe1aaee526ba008424c3ec33e41d312
asrivastava <asrivastava@aconex.com>
2016-05-16 16:29:24 +0530

    Add dist folder and update package.json to run scaffold

---
+++ dist/index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _fsPath = require('fs-path');
+
+var _fsPath2 = _interopRequireDefault(_fsPath);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _parse = require('./parse');
+
+var _parse2 = _interopRequireDefault(_parse);
+
+var hardCodedName = '<%= templateName %>';
+var path = undefined;
+
+var createWriteStream = function createWriteStream(path, fileName) {
+    return _fs2['default'].createWriteStream(path + "/" + fileName);
+};
+
+var makeDirectory = function makeDirectory(dirName) {
+    _fsPath2['default'].mkdirSync(dirName, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+};
+
+var scaffold = {
+    run: function run(argv) {
+        var moduleName = argv._.pop();
+
+        if (argv.l) {
+            path = argv.l;
+        }
+
+        var parse = new _parse2['default'](moduleName);
+
+        makeDirectory(path);
+
+        _fsPath2['default'].find(__dirname + '/../template', function (err, list) {
+
+            if (err) {
+                console.log(err);
+            }
+
+            list.dirs.forEach(function (dirName) {
+                makeDirectory(path + "/" + dirName.split("/").pop());
+            });
+
+            list.files.forEach(function (file) {
+                var fileName = file.replace(process.cwd().toString() + "/template/", "");
+
+                if (fileName.indexOf('controllers') >= 0) {
+                    fileName = fileName.replace("Controller", moduleName + "Controller");
+                }
+
+                parse.replace(hardCodedName, file).pipe(createWriteStream(path, fileName));
+            });
+        });
+    }
+};
+
+exports['default'] = scaffold;
+module.exports = exports['default'];
---
+++ dist/parse.js
@@ -0,0 +1,48 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _replacestream = require('replacestream');
+
+var _replacestream2 = _interopRequireDefault(_replacestream);
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var Parse = (function () {
+    function Parse(replaceString) {
+        _classCallCheck(this, Parse);
+
+        this.replaceString = replaceString;
+    }
+
+    _createClass(Parse, [{
+        key: '_readFile',
+        value: function _readFile(fileName) {
+            return _fs2['default'].createReadStream(fileName);
+        }
+    }, {
+        key: 'replace',
+        value: function replace(templateString, file) {
+            return this._readFile(file).pipe((0, _replacestream2['default'])(templateString, this.replaceString));
+        }
+    }]);
+
+    return Parse;
+})();
+
+exports['default'] = Parse;
+module.exports = exports['default'];
